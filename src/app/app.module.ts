f04c896b52684c40695486346f9de00f69787222
Ben Li <huanli198@gmail.com>
2022-06-13 11:24:00 +1000

    add counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ItemsComponent } from './components/items/items.component';
 import { ItemComponent } from './components/item/item.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ItemsComponent,
-    ItemComponent
+    ItemComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
